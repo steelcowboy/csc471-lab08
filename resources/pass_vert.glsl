146c410b330272f3f003be4170e9955b413e9410
Jim Heald <james.r.heald@gmail.com>
2018-03-01 19:13:18 -0800

    Fixed indentation

--- resources/pass_vert.glsl
+++ resources/pass_vert.glsl
@@ -5,6 +5,6 @@ out vec2 texCoord;
 
 void main()
 {
-   gl_Position = vec4(vertPos, 1);
-	texCoord = (vertPos.xy+vec2(1, 1))/2.0;
+    gl_Position = vec4(vertPos, 1);
+    texCoord = (vertPos.xy+vec2(1, 1))/2.0;
 }
